5d8b1f66107873cd8e000d30cf2ffddfa8d02f8f
Sid Shankar <sid.shankar@prodigygame.com>
2020-12-05 23:37:27 -0500

    JS Solution for day 5 - improved - take 2

--- day5.js
+++ day5.js
@@ -887,11 +887,9 @@ const decodeColumn = boardingPass => {
 
 const getSeatId = (row, column) => row * 8 + column;
 
-const seatIds = boardingPasses.map((pass, index) => {
-  const row = decodeRow(pass);
-  const column = decodeColumn(pass);
-  return getSeatId(row, column);
-});
+const seatIds = boardingPasses.map(
+  boardingPass => getSeatId(decodeRow(boardingPass), decodeColumn(boardingPass))
+);
 
 seatIds.sort((a,b) => a - b);
 console.log(`Maximum seat ID is: ${seatIds[seatIds.length - 1]}`);
